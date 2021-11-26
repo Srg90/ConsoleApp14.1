8bac1149dc2a3c24342acd94110e6199e0c1c261
User <User@DESKTOP-3CUDF3C>
2021-11-26 23:45:02 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            Dog dog = new Dog();
+            cat.ShowInfo();
+            Console.WriteLine();
+            dog.ShowInfo();
+            
+            Console.ReadKey();
+        }
+    }
+    abstract class Animal
+    {
+
+        public abstract string Name { get; set; }
+
+        public Animal()
+        {
+            Name = "Копатыч";
+        }
+
+        public abstract void Say();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Привет, меня зовут {0}", Name);
+            Say();
+
+        }
+                        
+    }
+
+    class Cat : Animal
+    {
+        public override string Name { get; set; }
+
+      
+        public override void Say()
+        {
+            Console.WriteLine("Мяу мяу");
+        }
+                     
+    }
+
+    class Dog : Animal
+    {
+        public override string Name { get; set; }
+
+     
+        public override void Say()
+        {
+            Console.WriteLine("Гав гав");
+        }
+
+    }
+
+}
